aa0aa17edcf363752b947096175e1f4845e1e2ae
singhyuvi712 <yuvraj.singh@cogoport.com>
2023-02-14 12:37:42 +0530

    two files here

--- file1.rb
+++ file1.rb
@@ -1,3 +1,7 @@
 class Apnirepo<Apnirep
 
+  def life
+    self.errors.add(:this, "life is hard")
+  end
+
 end
--- file2.rb
+++ file2.rb
@@ -1,3 +1,7 @@
-class Apnirepo2<Apnirep2
+class Apnirepo2 < Apnirep2
+
+  def life
+    self.errors.add(:this, "life is hard")
+  end
 
 end
